8fbc76ae9167acfbe713f05c65ff21fec5047947
rizky redjo <celebgramme.dev@gmail.com>
2019-03-19 15:53:49 +0700

    test

--- resources/views/emails/confirm-email.blade.php
+++ resources/views/emails/confirm-email.blade.php
@@ -1,7 +1,6 @@
 Terimakasih atas registrasinya di Omnifluencer <br>
 <br>
 Silahkan klik link dibawah untuk mengkonfirmasi registrasi Anda <br>
-<STRONG><a href="{{$url}}"> Link Confirmation </a></STRONG> <br>
 <br>
 <br>
 Terima kasih,<br>
