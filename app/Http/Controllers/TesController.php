bbfc72c6c4267fefe8881132437b22fcdb7c14cc
rizky redjo <celebgramme.dev@gmail.com>
2019-04-22 17:30:28 +0700

    test

--- app/Http/Controllers/TesController.php
+++ app/Http/Controllers/TesController.php
@@ -27,8 +27,8 @@ class TesController extends Controller
       do {
         // $url2 = "http://cmx.space/get-user-feed/".$arr_res["username"].'/'.$end_cursor;
         // $arr_res2 = AccountController::igcallback($url2);    
-        dd(InstagramHelper::get_user_feed($arr_res["username"],$end_cursor));
-        $arr_res2 = json_decode(InstagramHelper::get_user_feed($arr_res["username"],$end_cursor),true);
+        // dd(InstagramHelper::get_user_feed($arr_res["username"],$end_cursor));
+        $arr_res2 = InstagramHelper::get_user_feed($arr_res["username"],$end_cursor);
 
         // $url3 = "http://cmx.space/get-user-feed-maxid/".$arr_res["username"].'/'.$end_cursor;
         // $arr_res3 = AccountController::igcallback($url3,'string');
@@ -46,6 +46,7 @@ class TesController extends Controller
           }
 
           foreach ($arr_res2 as $arr) {
+            $arr = json_decode($arr,true);
             if($count>=20){
               break;
             } else {
