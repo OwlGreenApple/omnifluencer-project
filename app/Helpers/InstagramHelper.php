b33243d74f520d98cb9fab5fe0b8a384868d9fdb
rizky redjo <celebgramme.dev@gmail.com>
2019-09-25 14:30:26 +0700

    fixing

--- app/Helpers/InstagramHelper.php
+++ app/Helpers/InstagramHelper.php
@@ -618,7 +618,8 @@ class InstagramHelper
         
         // $i->setProxy('http://michaelsugih:TUhmQPS2erGtEe2@id.smartproxy.io:10001');
         // $i->login("mayymayyaa", "qwerty12345", 300);
-        $i->setProxy("http://".$arr_user['proxy'].":".$arr_user['port']);
+        // $i->setProxy("http://".$arr_user['proxy'].":".$arr_user['port']);
+        $i->setProxy("http://".$arr_user['username'].":".$arr_user['password']."@".$arr_user['proxy'].":".$arr_user['port']);
         $i->login($arr_user["username"], $arr_user["password"], 300);
       }
       
