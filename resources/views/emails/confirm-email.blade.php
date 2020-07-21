6c5b8c1f39b15684aa2ec110baa2590b09bf93d7
rizky redjo <celebgramme.dev@gmail.com>
2020-07-21 14:57:28 +0700

    info email

--- resources/views/emails/confirm-email.blade.php
+++ resources/views/emails/confirm-email.blade.php
@@ -14,3 +14,12 @@ Silahkan klik link dibawah untuk mengkonfirmasi registrasi Anda <br>
 Terima kasih,<br>
 <br>
 Omnifluencer
+
+<br>
+<br>
+Jika ada yang ingin ditanyakan,<br>
+Silakan hubungi support kami di info@activomni.com <br>
+Pesan Anda akan kami balas maximal 1x24 jam kerja. <br>
+<br>
+Atau Anda juga bisa menghubungi support kami di WA 0817-318-368 <br>
+Pada jam kerja, Senin s/d Jumat jam 08.00-17.00<br>
